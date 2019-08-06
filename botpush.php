dbdc4ad5b2d32805c22f2297a3ce83d5e85673ec
kittisak-cdv <52478703+kittisak-cdv@users.noreply.github.com>
2019-08-06 17:48:28 +0700

    c

--- botpush.php
+++ botpush.php
@@ -8,7 +8,7 @@ $access_token = '7EgwZlW//QEpWcqRToKg9j72f/Hv93iXso8LVv4N1YME8J11uIUh2H12p7ugSWm
 
 $channelSecret = '26ade74fb9e50f7c40245e4d3ef0d2f3';
 
-$pushID = 'Ucc61e47137ffb649253c80fed64778f7';
+$pushID = 'Ub9b7c347be49f593745c237d7e7cf497';
 
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
